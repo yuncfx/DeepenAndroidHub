6b6da6dae0bc620313850a143bc1398748cd3741
p_shanemao <shanemao@tinnove.com.cn>
2021-09-24 21:53:01 +0800

    IssueID=20210924 update trimIndent & trimMargin
    Signed-off-by: p_shanemao <shanemao@tinnove.com.cn>

--- demo/src/main/kotlin/com/shengsiyuan/chapter1/HelloKotlin6.kt
+++ demo/src/main/kotlin/com/shengsiyuan/chapter1/HelloKotlin6.kt
@@ -18,4 +18,29 @@ fun main(args: Array<String>) {
         println("array[$index]: $value")
     }
 
+    println("-----")
+
+    val price = """
+${'$'}_9.99
+"""
+    println(price)
+
+    println("-----")
+
+    val text = """
+     |理论是你知道是这样，但它却不好用。
+     |实践是它很好用，但你不知道是为什么。
+     |程序员将理论和实践结合到一起：
+     |既不好用，也不知道是为什么。
+   """
+    println(text.trimMargin())
+
+    println("-----")
+    val text2 = """
+           |hello
+       |world!
+   """
+    println(text2.trimIndent())
+    println(text2.trimMargin())
+
 }
