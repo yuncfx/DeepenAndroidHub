316977f6397bbcc7c8fd6fa273fe7660b16cd31d
p_shanemao <shanemao@tinnove.com.cn>
2021-11-18 21:37:49 +0800

    IssueID=20211118 add exception demo
    Signed-off-by: p_shanemao <shanemao@tinnove.com.cn>

---
+++ demo/src/main/kotlin/com/shengsiyuan/chapter7/AnnotationDemo.kt
@@ -0,0 +1,36 @@
+package com.shengsiyuan.chapter7
+
+/*
+    P34 注解(annotation)
+    Java中注解 @interface
+ */
+// meta-annotation (元注解)
+
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.EXPRESSION,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.PROPERTY_SETTER
+)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+annotation class MyAnnotation
+
+@MyAnnotation
+class MyClass {
+    @MyAnnotation
+    fun myMethod(@MyAnnotation a: Int): Int {
+        return (@MyAnnotation 10)
+    }
+}
+
+class MyClass2 @MyAnnotation constructor(a: Int) {
+
+}
+
+class MyClass3 {
+    var a: Int? = null
+        @MyAnnotation set
+}
