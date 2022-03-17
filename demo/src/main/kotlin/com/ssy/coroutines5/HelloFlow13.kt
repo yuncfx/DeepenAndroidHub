c286b19cfd29b5706c8053043acd637a90c4d9c1
shanemao <shanemao@tinnove.com.cn>
2022-03-18 00:04:45 +0800

    IssueID=20220318 update flow concept (V2)
    Signed-off-by: shanemao <shanemao@tinnove.com.cn>

--- demo/src/main/kotlin/com/ssy/coroutines5/HelloFlow13.kt
+++ demo/src/main/kotlin/com/ssy/coroutines5/HelloFlow13.kt
@@ -1,14 +1,19 @@
 package com.ssy.coroutines5
 
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Flow Context （Flow上下文）
  *
  * Flow的收集动作总是发生在调用协程的上下文当中, 这个特性叫做Context Preservation（上下文保留）
+ * 所以默认情况下， 产生flow流的地方，也运行在collect执行的上下文中，以避免资源切换的浪费
+ *
+ * This is the perfect default for fast-running or asynchronous code that does not care about
+ * the execution context and does not block the caller.
  */
 private fun log(msg: String) = println("[${Thread.currentThread().name}, $msg]")
 
@@ -22,5 +27,11 @@ private fun myMethod(): Flow<Int> = flow {
 fun main() = runBlocking {
     myMethod().collect {
         log("Collected: $it")
+    }
+
+    withContext(Dispatchers.IO) {
+        myMethod().collect {
+            log("Collected 2: $it")
+        }
     }
 }
