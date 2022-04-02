e01ebf13f8fd9292df1aced022a0713c1c940481
p_shanemao <shanemao@tinnove.com.cn>
2022-04-02 19:53:47 +0800

    IssueID=20220402 fix demo compile error
    Signed-off-by: p_shanemao <shanemao@tinnove.com.cn>

--- demo/src/main/kotlin/com/ssy/coroutines5/FlowException.kt
+++ demo/src/main/kotlin/com/ssy/coroutines5/FlowException.kt
@@ -10,19 +10,19 @@ import kotlinx.coroutines.runBlocking
  * flow的异常 从发射阶段，中间阶段 终止阶段都可以捕获异常。
  */
 private fun myMethod(): Flow<String> = flow {
-    for (i in 1..3) {
-        println("Emitting $i")
-        emit(i)
-    }
+  for (i in 1..3) {
+    println("Emitting $i")
+    emit(i)
+  }
 }.map { value ->
-    check(value <= 1) { "Crash on $value" }
-    "string $value"
+  check(value <= 1) { "Crash on $value" }
+  "string $value"
 }
 
 fun main() = runBlocking {
-    try {
-        myMethod().collect { println(it) }
-    } catch (e: Throwable) {
-        println("Caught $e")
-    }
+  try {
+    myMethod().collect { println(it) }
+  } catch (e: Throwable) {
+    println("Caught $e")
+  }
 }
